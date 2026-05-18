e01bd3c037b3b946cf5b045ea3a875e88a462138
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2026-05-18 18:54:16 -0400

    Change estimator to 30 to maintain git file size limit

--- train_model.py
+++ train_model.py
@@ -19,7 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Train
-model = RandomForestRegressor(n_estimators=80, random_state=42)
+model = RandomForestRegressor(n_estimators=30, random_state=42)
 model.fit(X_train, y_train)
 
 # Evaluate
